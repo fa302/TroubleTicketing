f2de95901a467042fe5e484d1cc5fdd83b710d78
fa302 <75071457+fa302@users.noreply.github.com>
2023-02-20 14:40:50 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Helpdesk Trouble Ticketing System</title>
+</head>
+	
+<body>
+<center><marquee><p>Crowe Liberia Trouble Ticketing System</p></marquee></center>
+	<?php
+	echo 'hello';
+	?>
+	
+</body>
+</html>
